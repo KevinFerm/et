7294260de1bf6e56bf1a432dfa9427f6598b8e66
Kevin Ferm <kevinferm@401FermK.lan>
2016-12-04 14:47:28 +0100

    testing

--- plugins/se.infomaker.npwriterdevkit/DevKitComponent.js
+++ plugins/se.infomaker.npwriterdevkit/DevKitComponent.js
@@ -1,5 +1,5 @@
 import {Component} from 'substance'
-
+const {api, event} = writer
 class DevKitComponent extends Component {
 
     /**
@@ -15,6 +15,12 @@ class DevKitComponent extends Component {
      */
     constructor(...args) {
         super(...args)
+        
+    }
+    callback(error,response,body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+        }
     }
 
 
@@ -27,6 +33,36 @@ class DevKitComponent extends Component {
             clickCount: 0
         }
     }
+    getContent() {
+        var nodes = api.document.getDocumentNodes()
+        var textContent = "";
+        nodes.forEach(function (node) {
+            if (node.content) {
+                textContent += node.content.trim()
+            }
+        })
+        return textContent
+    }
+//curl -X POST --include 'https://twinword-sentiment-analysis.p.mashape.com/analyze/' \
+
+    getTextSentiments(text) {
+        let url = 'https://twinword-sentiment-analysis.p.mashape.com/analyze/'
+        api.router.get('/api/proxy/', {url: url,
+            text: text,
+            headers: {
+                'X-Mashape-Key': "2dryu65QD0msh8bFx7G3V43btP2up135YljjsnOqWFQwzaLGOK",
+                "Accept": "application/json"
+            }
+        })
+        .then(response => api.router.checkForOKStatus(response))
+        .then(response => response.json())
+        .then(json => {
+            console.log("Response is", json)
+        })
+        .catch((e) => {
+            console.error(e)
+        })
+    }
 
 
 
@@ -37,6 +73,9 @@ class DevKitComponent extends Component {
      */
     render($$) {
         const el = $$('div').addClass('devkit')
+        let data = this.getContent()
+        let sentiments = this.getTextSentiments(data)
+        console.log(sentiments)
 
         el.append($$('h2').append(this.getLabel('Devkit plugin loaded')))
         el.append($$('p').append(String(this.state.clickCount)))
